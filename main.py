7739ae1c53fd118cea3ca90caf68c52f594b6bb4
Jakub Zurakowski <Jakub_zzzz@hotmail.com>
2020-08-12 22:15:58 +0100

    Updated main with various demos / tests

--- main.py
+++ main.py
@@ -2,21 +2,52 @@ from LinkedList import (Node, LinkedList)
 import random
 
 
+def sep():
+    print("===========================\n")
+
+
 def main():
     "Main entrypoint for the app"
 
-    a = [Node(i) for i in range(0, 1000000)]
+    # Create a list of Nodes
+    nodeList = [Node(i) for i in range(0, 10)]
 
-    ll = LinkedList(a[0])
-    for node in a[1:]:
-        ll.push(node)
+    # Create the Linked List and add Nodes to it
+    linkedList = LinkedList(nodeList[0])
+    for node in nodeList[1:]:
+        linkedList.push(node)
 
-    ll.insert(0, Node('start'))
-    ll.insert(5, Node('mid'))
-    ll.insert(len(ll), Node('end'))
-    ll.insert(random.randint(0, 1000000), Node('Rand'))
+    print(f"List: {linkedList}")
+    sep()
 
-    print(ll.find('Rand'))
+    # Insert some Nodes
+    linkedList.insert(0, Node('start'))
+    linkedList.insert(5, Node('mid'))
+    linkedList.insert(len(linkedList), Node('end'))
+    linkedList.insert(random.randint(0, 10), Node('Rand'))
+
+    print(f"List: {linkedList}")
+    sep()
+
+    # Pop some Nodes
+    print(f"Pop(): {linkedList.pop()}")
+    print(f"Remove(5): {linkedList.remove(5)}")
+
+    print(f"List: {linkedList}")
+    sep()
+
+    # Find the random Node
+    print(f"Find('Rand'): {linkedList.find('Rand')}")
+
+    print(f"List: {linkedList}")
+    sep()
+
+    # Remove the random Node
+
+    print(f"Remove(Rand): {linkedList.remove(linkedList.find('Rand')[0])}")
+
+    print(f"List: {linkedList}")
+    sep()
 
 
 main()
